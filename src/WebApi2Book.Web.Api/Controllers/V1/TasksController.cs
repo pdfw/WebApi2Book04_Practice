f41ed3129e40606c58fab5ea68d3e5f4a19cf45d
pdfw <pdfw2@163.com>
2016-06-03 15:00:46 +0800

    Add

--- src/WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
+++ src/WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Web.Http;
+using WebApi2Book.Web.Common.Routing;
+using WebApi2Book.Web.Api.Models;
 using System.Net.Http;
-using System.Web.Http;
 
 namespace WebApi2Book.Web.Api.Controllers.V1
 {
+    [ApiVersion1RoutePrefix("tasks")]
     public class TasksController : ApiController
     {
+        [Route("", Name = "AddTaskRoute")]
+        [HttpPost]
+        public Task AddTask(HttpRequestMessage requestMessage, Task newTask)
+        {
+            return new Task
+            {
+                Subject = "In v1, newTask.Subject = " + newTask.Subject
+            };
+        }
     }
 }
