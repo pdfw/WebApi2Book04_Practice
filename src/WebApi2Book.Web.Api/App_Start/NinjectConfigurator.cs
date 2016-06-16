9d55f5a28003d2f37cd090f3f98280795eadf17d
pdfw <pdfw2@163.com>
2016-06-16 18:20:05 +0800

    add

--- src/WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
+++ src/WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
@@ -9,6 +9,7 @@ using Ninject.Web.Common;
 using WebApi2Book.Common;
 using WebApi2Book.Common.Logging;
 using WebApi2Book.Data.SqlServer.Mapping;
+using WebApi2Book.Web.Common;
 
 namespace WebApi2Book.Web.Api
 {
@@ -43,6 +44,7 @@ namespace WebApi2Book.Web.Api
             .BuildSessionFactory();
             container.Bind<ISessionFactory>().ToConstant(sessionFactory);
             container.Bind<ISession>().ToMethod(CreateSession).InRequestScope();
+            container.Bind<IActionTransactionHelper>().To<ActionTransactionHelper>().InRequestScope();
         }
 
         private ISession CreateSession(IContext context)
