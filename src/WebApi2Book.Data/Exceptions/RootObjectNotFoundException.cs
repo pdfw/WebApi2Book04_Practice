b4af564af2f22b0362c3cb75080d4a938374a1f7
pdfw <pdfw2@163.com>
2016-06-29 01:02:55 +0800

    Added

---
+++ src/WebApi2Book.Data/Exceptions/RootObjectNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApi2Book.Data.Exceptions
+{
+    [Serializable]
+    public class RootObjectNotFoundException : Exception
+    {
+        public RootObjectNotFoundException(string message)
+            : base(message)
+        {
+        }
+    }
+}
