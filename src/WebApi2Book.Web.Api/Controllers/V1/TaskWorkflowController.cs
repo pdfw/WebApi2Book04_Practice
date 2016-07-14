44ac06cf2ee86b206df9a7283c39cf4814a86e13
pdfw <pdfw2@163.com>
2016-07-14 20:11:25 +0800

    add

--- src/WebApi2Book.Web.Api/Controllers/V1/TaskWorkflowController.cs
+++ src/WebApi2Book.Web.Api/Controllers/V1/TaskWorkflowController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using WebApi2Book.Common;
 using WebApi2Book.Web.Api.MaintenanceProcessing;
 using WebApi2Book.Web.Api.Models;
@@ -14,21 +9,32 @@ namespace WebApi2Book.Web.Api.Controllers.V1
 {
     [ApiVersion1RoutePrefix("")]
     [UnitOfWorkActionFilter]
+    [Authorize(Roles = Constants.RoleNames.SeniorWorker)]
     public class TaskWorkflowController : ApiController
     {
         private readonly IStartTaskWorkflowProcessor _startTaskWorkflowProcessor;
+        private readonly ICompleteTaskWorkflowProcessor _completeTaskWorkflowProcessor;
 
-        public TaskWorkflowController(IStartTaskWorkflowProcessor startTaskWorkflowProcessor)
+        public TaskWorkflowController(IStartTaskWorkflowProcessor startTaskWorkflowProcessor,
+        ICompleteTaskWorkflowProcessor completeTaskWorkflowProcessor)
         {
             _startTaskWorkflowProcessor = startTaskWorkflowProcessor;
+            _completeTaskWorkflowProcessor = completeTaskWorkflowProcessor;
         }
 
         [HttpPost]
-        [Authorize(Roles = Constants.RoleNames.SeniorWorker)]
         [Route("tasks/{taskId:long}/activations", Name = "StartTaskRoute")]
         public Task StartTask(long taskId)
         {
             var task = _startTaskWorkflowProcessor.StartTask(taskId);
+            return task;
+        }
+
+        [HttpPost]
+        [Route("tasks/{taskId:long}/completions", Name = "CompleteTaskRoute")]
+        public Task CompleteTask(long taskId)
+        {
+            var task = _completeTaskWorkflowProcessor.CompleteTask(taskId);
             return task;
         }
     }
