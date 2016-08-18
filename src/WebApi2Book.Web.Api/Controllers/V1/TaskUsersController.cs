cc99eb44667ffe4a13cadff2be6e7d6bbf29d855
pdfw <pdfw2@163.com>
2016-08-19 00:20:52 +0800

    a

---
+++ src/WebApi2Book.Web.Api/Controllers/V1/TaskUsersController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using WebApi2Book.Common;
+using WebApi2Book.Web.Common;
+using WebApi2Book.Web.Common.Routing;
+
+namespace WebApi2Book.Web.Api.Controllers.V1
+{
+    [ApiVersion1RoutePrefix("tasks")]
+    [UnitOfWorkActionFilter]
+    [Authorize(Roles = Constants.RoleNames.SeniorWorker)]
+    public class TaskUsersController : ApiController
+    {
+
+    }
+}
