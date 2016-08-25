aa3da1d27505a83e5cef64fed0787873c915c57b
pdfw <pdfw2@163.com>
2016-08-26 00:06:50 +0800

    a

--- src/WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
+++ src/WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
@@ -50,6 +50,7 @@ namespace WebApi2Book.Web.Api
             container.Bind<IUpdateablePropertyDetector>().To<JObjectUpdateablePropertyDetector>().InSingletonScope();
             container.Bind<IUpdateTaskMaintenanceProcessor>().To<UpdateTaskMaintenanceProcessor>()
 .InRequestScope();
+            container.Bind<IPagedDataRequestFactory>().To<PagedDataRequestFactory>().InSingletonScope();
         }
 
         private void ConfigureLog4net(IKernel container)
